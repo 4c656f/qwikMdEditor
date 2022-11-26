2cfa94b7142d7141ee5471ec5a77045b3fb9dc1f
4c656f <vlevo.yovlyo@yandex.ru>
2022-11-26 19:29:06 +0600

    run linter, add default components, add context

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,60 @@
+import {component$, HTMLAttributes, PropFunction, Slot, useStylesScoped$} from '@builder.io/qwik';
+
+import styles from './button.scss?inline';
+import {ButtonType} from "../../../../types/IElementType";
+import {IElementsSize} from "../../../../types/IElementsSize";
+import {IColorIndex} from "../../../../types/IColorIndex";
+
+
+const type = 'button'
+
+type ButtonProps = {
+    onClick$?: PropFunction<() => void>;
+    type?: ButtonType;
+    size?: IElementsSize;
+    colorIndex?: IColorIndex;
+    className?: string;
+} & Omit<HTMLAttributes<typeof type>, 'children'>
+
+
+export default component$((props: ButtonProps) => {
+
+    const {
+        className,
+        onClick$,
+        type = 'contained',
+        size = 'medium',
+        colorIndex = '0',
+        ...rest
+    } = props
+
+
+    useStylesScoped$(styles);
+
+    const classes = [
+        className ? className : '',
+        'container',
+        type,
+        size,
+        `color_${colorIndex}_index`,
+    ]
+
+    return (
+        <button
+            onClick$={async () => {
+                if (onClick$) await onClick$();
+            }}
+            class={classes.join(' ')}
+            {...rest}
+        >
+            <Slot/>
+            <div
+                class={'icon'}
+            >
+                <Slot
+                    name={'icon'}
+                />
+            </div>
+        </button>
+    )
+})
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,20 +1,40 @@
-import { component$, Slot } from '@builder.io/qwik';
+import {component$, createContext, Slot, useClientEffect$, useContextProvider, useStore} from '@builder.io/qwik';
 import Header from '../components/header/header';
 
+
+type globalStore = {
+
+    isDark: boolean;
+}
+export const globalContext = createContext<globalStore>('globalContext')
+
 export default component$(() => {
-  return (
-    <>
-      <main>
-        <Header />
-        <section>
-          <Slot />
-        </section>
-      </main>
-      <footer>
-        <a href="https://www.builder.io/" target="_blank">
-          Made with ♡ by Builder.io
-        </a>
-      </footer>
-    </>
-  );
+
+    const globalStore = useStore<globalStore>({
+        isDark: true
+    })
+
+    useContextProvider(globalContext, globalStore)
+
+
+    useClientEffect$(({track}) => {
+        track(() => globalStore.isDark)
+        document.body.dataset.theme = globalStore.isDark ? 'dark' : 'light'
+    })
+
+    return (
+        <>
+            <main>
+                <Header/>
+                <section>
+                    <Slot/>
+                </section>
+            </main>
+            <footer>
+                <a href="https://www.builder.io/" target="_blank">
+                    Made with ♡ by Builder.io
+                </a>
+            </footer>
+        </>
+    );
 });
